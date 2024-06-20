ec8988f91a7f7dcb6708a1cbee2653aa2cd48f94
Tonie Victor <victornwanochi@gmail.com>
2024-06-20 04:56:55 +0000

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,29 @@
-#!/usr/bin/env python3
-""" Redis Module """
-
+from functools import wraps
 import redis
 import requests
-from functools import wraps
+from typing import Callable
 
-r = redis.Redis()
+redis_ = redis.Redis()
 
 
-def url_access_count(method):
-    """decorator for get_page function"""
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
     return wrapper
 
 
-@url_access_count
+@count_requests
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
